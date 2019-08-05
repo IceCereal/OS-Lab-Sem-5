7b4cd90b092698dc04625460d953e67010afcf0c
IceCereal <ns_raghav@hotmail.com>
2019-08-05 16:39:26 +0530

    Add Main + Initial Floof | Not Working

--- 2-5-Aug/Prgm-1.c
+++ 2-5-Aug/Prgm-1.c
@@ -28,3 +28,65 @@
 		The End.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	// CLI:
+	// argv[1] - FILENAME
+
+	if (argc != 2){
+		printf("\nOnly two arguments can be taken! ./a.out FILENAME.\nExiting\n");
+		return -1;
+	}
+
+	FILE *file_ptr;
+	file_ptr = fopen(argv[1], "r");
+
+	if (file_ptr == NULL){
+		printf("\nFile Not Found!\nExiting\n");
+		return -1;
+	}
+
+	char line[200];
+
+	while ( fgets(line, 1000, file_ptr) != NULL ){
+		char *args[20];
+		char *token;
+		char *command;
+		int count = 0;
+
+		printf("%s", line);
+
+		token = strtok(line, " ");
+
+		while (token != NULL){
+			printf("%s ", token);
+			if (count == 0){
+				command = token;
+				args[count] = token;
+			}
+			else
+				args[count] = token;
+			count += 1;
+			token = strtok(NULL, " ");
+		}
+		args[count] = NULL;
+
+		for (int i = 0; i < count; ++i)
+			printf("%s ", args[count]);
+
+		pid_t child_pid = fork();
+
+		if (child_pid == 0)
+			execvp(command, args);
+		else
+			wait(NULL);
+	}
+
+	fclose(file_ptr);
+	return 0;
+}
